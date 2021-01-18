d8036a81aaa81c293a6c634cde867ee327412d8d
wayee <378772944@qq.com>
2021-01-18 14:56:12 +0800

    404 to baby go home

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>宝贝回家</title>
+    <script type="text/javascript" src="//qzonestyle.gtimg.cn/qzone/hybrid/app/404/search_children.js" charset="utf-8"
+            homePageUrl="https://www.ewayee.com" homePageName="回到我的主页"></script>
+</head>
+<body>
+
+</body>
+</html>
