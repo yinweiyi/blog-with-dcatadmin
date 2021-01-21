91db15d48e27497f9cf7750d22cd16b3d52b4c57
wayee <378772944@qq.com>
2021-01-21 17:29:46 +0800

    接入谷歌广告联盟

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -22,7 +22,9 @@
     <script src="{{ asset('js/ie-emulation-modes-warning.js') }}"></script>
     <!--[if lt IE 9]>
     <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script> <![endif]--> </head>
+    <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script> <![endif]-->
+    <script data-ad-client="ca-pub-6811262025296014" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+</head>
 <body>
 @include('layouts.nav')
 <div class="container main">
