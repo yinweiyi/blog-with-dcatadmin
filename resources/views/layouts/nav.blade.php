34386c4cd96cd781767b9f6d7e693422967a6061
wayee <378772944@qq.com>
2021-02-04 16:04:31 +0800

    title

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -4,7 +4,7 @@
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
                     data-target="#bs-navbar-collapse" aria-expanded="false"><span class="sr-only">下拉菜单</span> <span
                     class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></button>
-            <a href="{{ route('home.index') }}" class="navbar-brand">{{ $configs['title'] ?? '博客' }} | 技术博客</a></div>
+            <a href="{{ route('home.index') }}" class="navbar-brand">{{ $configs['title'] ?? '博客' }} | {{$configs['sub_title'] ?? ''}}</a></div>
         <div class="collapse navbar-collapse" id="bs-navbar-collapse">
             <ul class="nav navbar-nav top-navbar-nav">
                 <li class="{{ request()->is('/') ? 'active' : '' }}"><a href="{{ route('home.index') }}">首页</a></li>
