d44cd64fd0df5ba4c4381bbeaa2644823075a8c6
wayee <378772944@qq.com>
2021-03-25 17:09:34 +0800

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -9,7 +9,7 @@
     <p> Copyright © 2010-2020
         访问 : {{ $visitCount }} 次
         <a href="{{ route('home.index') }}">{{ $configs['title'] ?? '' }}</a>
-        <a href="https://www.beian.miit.gov.cn/" target="_blank" rel="nofollow">{{ $configs['icp'] ?? '' }}</a>
+        <a href="https://beian.miit.gov.cn" target="_blank" rel="nofollow">{{ $configs['icp'] ?? '' }}</a>
 
         @if($configs['beian'])
             <img style="display: inline-block; margin-left: 5px" src="{{ asset('images/beian.png') }}"/>
